a94591d86915b27edb8f1ad937ec381540138319
GregorGall <GregorGall50@gmail.com>
2023-01-24 12:51:49 +0300

    Cell class implementation
    Cells has neighbours and can be killed or renewed

--- include/Cell.h
+++ include/Cell.h
@@ -1,4 +1,28 @@
-#ifndef CELL_H
-#define CELL_H
+#pragma once
 
-#endif // CELL_H
+namespace Life {
+
+  class Cell {
+
+  public:
+
+    Cell(){};
+
+    operator bool() const { return status; }
+
+    void kill(){ status = false; };
+
+    void renew(){ status = true; };
+
+    void increment(){ ++neighbours; };
+
+    void evolute() { neighbours == 3 || neighbours == 2 ? status = true : status = false; };
+
+  private:
+
+    bool status{ false };
+    int neighbours{ 0 };
+
+  };
+
+}
--- test/mainTest.cpp
+++ test/mainTest.cpp
@@ -1,9 +1,34 @@
-#include <iostream>
 #include "Desk.h"
+#include <iostream>
+
+class View{
+
+public:
+
+  void operator<<(const Life::Desk& desk){
+    for(int i = 0; i < desk.rows(); ++i){
+      for(int j = 0; j < desk.cols(); ++j){
+        std::cout << desk(i, j);
+      }
+      std::cout << std::endl;
+    }
+  }
+
+};
+
 
 int main(){
 
-  std::cout << "Hello World\n";
+  Life::Desk d(10, 10);
+
+  auto neighbourList = d.getNeighbours(5,5);
+
+  for(auto cellPtr: neighbourList){
+    cellPtr->renew();
+  }
+
+  View v;
+  v << d;
 
   return 0;
 }
